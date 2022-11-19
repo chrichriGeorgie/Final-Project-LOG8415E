a4909830370c0a76568de69a11bf257520bccf5f
Christophe St-Georges <christophe.st-georges@polymtl.ca>
2022-11-19 12:09:24 -0500

    Edited header comment format

--- demo-script/main.sh
+++ demo-script/main.sh
@@ -1,10 +1,13 @@
-#The current script is inspired by the solution created by my team during the first assignment of LOG8415E. See this repo for the original code: https://github.com/chrichriGeorgie/Lab1-LOG8415E
+#
+# The current script is inspired by the solution created by my team during the first assignment of LOG8415E. 
+# See this repo for the original code: https://github.com/chrichriGeorgie/Lab1-LOG8415E
+#
 
 #!/bin/bash
-#setting working directory
+# Setting working directory
 cd "$(dirname "$0")"
 
-#AWS credentials configuration
+# AWS credentials configuration
 echo AWS Access Key ID:
 read aws_access_key_id
 
@@ -18,13 +21,13 @@ aws configure set aws_access_key_id $aws_access_key_id
 aws configure set aws_secret_access_key $aws_secret_access_key
 aws configure set aws_session_token $aws_session_token
 
-#Terraform deploying AWS Infrastructure
+# Terraform deploying AWS Infrastructure
 cd ../instance-creation
 terraform init
 terraform apply -auto-approve
 cd ..
 
-#Clean up instances
+# Terraform cleaning up instances
 cd ../instance-creation
 echo Cleaning instances...
 rm -f destroy.txt
