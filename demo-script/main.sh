ba6b5259e768eb6a73dbc5b7436ceb88536cc02b
Christophe St-Georges <christophe.st-georges@polymtl.ca>
2022-11-19 12:02:17 -0500

    Added the bash code to deploy AWS instances. Inspired by Lab 1

---
+++ demo-script/main.sh
@@ -0,0 +1,33 @@
+#The current script is inspired by the solution created by my team during the first assignment of LOG8415E. See this repo for the original code: https://github.com/chrichriGeorgie/Lab1-LOG8415E
+
+#!/bin/bash
+#setting working directory
+cd "$(dirname "$0")"
+
+#AWS credentials configuration
+echo AWS Access Key ID:
+read aws_access_key_id
+
+echo AWS Secret Access Key:
+read aws_secret_access_key
+
+echo AWS Session Token:
+read aws_session_token
+
+aws configure set aws_access_key_id $aws_access_key_id
+aws configure set aws_secret_access_key $aws_secret_access_key
+aws configure set aws_session_token $aws_session_token
+
+#Terraform deploying AWS Infrastructure
+cd ../instance-creation
+terraform init
+terraform apply -auto-approve
+cd ..
+
+#Clean up instances
+cd ../instance-creation
+echo Cleaning instances...
+rm -f destroy.txt
+terraform destroy -auto-approve > destroy.txt
+echo Done!
+cd ..
